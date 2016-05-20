1c458b101563067d2ffd8570aef9e7b1851bbe44
sergeyradkov <radkovsn@gmail.com>
2016-05-20 16:45:01 -0600

    123

--- server-assets/db/models/team.js
+++ server-assets/db/models/team.js
@@ -17,29 +17,44 @@
     })
     
     exports.getTeams = function (req, res, next) {
-        
-        getTeams().then(function (teams) {
-                res.json(teams);
-            }
-        );
+		if (req.params.id) {
+            getTeam(req.params.id).then(function(team) {
+                return res.json(team)
+            })
+        } else {
+            getTeams(req.query).then(function(team) {
+                return res.json(teams);
+            })
+        }
     }
     function getTeams(){
-        var params = {};
-        var options = {};
+        var params = query;
+        var options = {with: ['player']};
         return Team.findAll(params, options);
     }
     
-    exports.addTeam = function (req, res, next) {
-        
-		addLeague(req.body).then(function (team) {
-				return res.json(team);
-			}
-		);
+    exports.getTeam = function(req, res, net) {
+		getTeam(req.params.id).then(function(team){
+			console.log(team);
+			return res.json(team[0]);
+		})
+	}
+    function getTeam(){
+        var options = {with: ['player']};
+            return Team.findAll(params, options);
     }
     
-    
-    
-    
-    
-    
+	exports.addTeam = function (req, res, next) {
+		addTeam(req.body.name, req.body.description).then(function (team) {
+				return res.json(team);
+			});
+	}
+    function addTeam (name, description){
+        return Team.create({
+            id: uuid.v4(),
+            name: name,
+            description: description
+        });
+    }
+
 })();
